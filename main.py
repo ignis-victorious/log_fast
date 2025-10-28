e0bdd1130cda8e500e60a3830d48a85e1d8e6f09
Emmanuele Levi <ignisvictorious@gmail.com>
2025-10-28 12:02:27 +0000

    Initial commit - starting code

---
+++ main.py
@@ -0,0 +1,30 @@
+#
+#  Import LIBRARIES
+import uvicorn
+from fastapi import FastAPI
+
+#  Import FILES
+#  # #
+
+
+app: FastAPI = FastAPI()
+
+
+@app.get(path="/")
+async def index() -> dict[str, str]:
+    return {"message": "Hello"}
+
+
+@app.get(path="/upload-videos")
+async def upload_videos() -> dict[str, str]:
+    return {"message": "Video Uploaded"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+#
+#  Import LIBRARIES
+
+#  Import FILES
+#  # #
